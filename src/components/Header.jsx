1db71a4121d15755901bce31680bf61146a18dd7
Khayalala <khayala.jafar@gmail.com>
2025-03-05 15:26:33 +0400

    App css changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,3 @@
-// import "../styles/Header.css";
-
-// const Header = () => {
-//   return (
-//     <header className="mystic-header">
-//       <div className="logo">
-//         <h1>Khayala's Celestial Insights</h1>
-//         <p className="tagline">
-//           “Your divine inheritance includes all of your earthly needs being met,
-//           as well as receiving all of your heart’s desires.”
-//         </p>
-//       </div>
-//       <nav className="nav-menu">
-//         <a href="#angel-numbers">Angel Numbers</a>
-//         <a href="#angelic-cards">Angelic Cards</a>
-//         <a href="#contact">Contact</a>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
 import "../styles/Header.css";
 
 const Header = () => {
